9c60a15d70f4f77f888f4f50a089d468f13e94b7
JamieHunstad <jamie.hunstad@gmail.com>
2022-09-27 20:17:48 -0500

    more styling - desktop and mobile layouts, toggable menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'marsApp';
+
+  forecastState: boolean = false;
+
+  toggleForecast(){
+    this.forecastState = !this.forecastState;
+    console.log(this.forecastState)
+  }
+
 }
