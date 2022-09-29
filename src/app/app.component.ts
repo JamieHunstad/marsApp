950be344345d3dfdff7bb1dadc5edd42ae0945a5
JamieHunstad <jamie.hunstad@gmail.com>
2022-09-29 17:55:28 -0500

    API CALL IS WORKINGgit add --allgit add --all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { WeatherService } from './services/weather.service';
 
 @Component({
@@ -7,36 +8,65 @@ import { WeatherService } from './services/weather.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+
+    
+  constructor(private weatherService: WeatherService) {
+
+  }
+  
   title = 'earthApp';
   toggleDateState: boolean = false;
   Data: any;
   Identifier: any;
   Image: any;
-  
-  constructor(private weatherService: WeatherService) {
-
-  }
-
-  ngOnInit(): void {
-    this.getCurrentDate();
-    this.weatherService.getWeatherData(this.myFullDate)
-      .subscribe({
-        next: (response) => {
-          this.Data = response;
-          this.Identifier = this.Data[0].identifier;
-
-          this.getImage(this.myDate.year, this.myDate.month, this.myDate.day, this.Identifier )
-
-        }
-      })
-
-  }
 
   myDate: any = {
     year: '',
     month: '',
     day: ''
   }
+  myFullDate: string = this.myDate.year + "-" + this.myDate.month + "-" + this.myDate.day
+
+
+
+  ngOnInit(): void {
+    this.getCurrentDate();
+    this.myFullDate = this.myDate.year + "-" + this.myDate.month + "-" + this.myDate.day
+    this.getImageData(this.myFullDate);
+
+  }
+
+
+  OnSubmit(form: NgForm){
+    console.log(form.value);
+    this.myDate.day = form.value.day.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+    });
+    this.myDate.month = form.value.month;
+    this.myDate.year = form.value.year;
+
+    this.myFullDate = this.myDate.year + "-" + this.myDate.month + "-" + this.myDate.day
+    this.getImageData(this.myFullDate)
+    
+    form.reset();
+  }
+
+
+private getImageData(date: string){
+  this.weatherService.getWeatherData(date)
+      .subscribe({
+        next: (response) => {
+          this.Data = response;
+
+          this.Identifier = this.Data[0].identifier;
+
+          this.getImage(this.myDate.year, this.myDate.month, this.myDate.day, this.Identifier)
+
+        }
+      })
+}
+
+
 
   getCurrentDate(){
     const today = new Date();
@@ -44,32 +74,37 @@ export class AppComponent implements OnInit{
     const month = today.getMonth();
     const year = today.getFullYear();
 
-    this.myDate.year = year.toString();;
+    this.myDate.year = year.toString();
 
     if (month < 10){
-      this.myDate.month = "0" + (month + 1).toString();
+      this.myDate.month = (month + 1).toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+    });
     } else if (month == 12){
       this.myDate.month = "12";
     } else {
-      this.myDate.month = (month + 1).toString();
+      this.myDate.month = (month + 1).toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+    });
     }
 
     if(day < 10){
-      this.myDate.day = "0" + (day - 1).toString();
+      this.myDate.day = (day - 1).toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+    });
     } else {
-      this.myDate.day = (day - 1).toString();
+      this.myDate.day = (day - 1).toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+    });
     }
-  }
 
+  }
 
 
   getImage(day: string, month: string, year: string, identifier: string){
     this.Image =  "https://api.nasa.gov/EPIC/archive/natural/"+day+"/"+month+"/"+year+"/png/epic_1b_"+identifier+".png?api_key=awx5urW97lj9lILIMBCKBeHo3YtSh2F6j9SjZkZx"
   }
 
-
-
-  myFullDate: string = this.myDate.year + "-" + this.myDate.month + "-" + this.myDate.day
 
   toggleDate(){
     this.toggleDateState = !this.toggleDateState;
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -10,11 +10,7 @@ export class WeatherService {
   constructor(private http: HttpClient) {}
 
   getWeatherData(myDate: string){
-      return this.http.get("https://api.nasa.gov/EPIC/api/natural/date"+myDate+"?api_key=awx5urW97lj9lILIMBCKBeHo3YtSh2F6j9SjZkZx")
-  }
-
-  getImage(day: string, month: string, year: string, identifier: string){
-    return this.http.get("https://api.nasa.gov/EPIC/archive/natural/"+day+"/"+month+"/"+year+"/png/epic_1b_"+identifier+".png?api_key=awx5urW97lj9lILIMBCKBeHo3YtSh2F6j9SjZkZx")
+      return this.http.get(("https://api.nasa.gov/EPIC/api/natural/date/" + myDate + "?api_key=awx5urW97lj9lILIMBCKBeHo3YtSh2F6j9SjZkZx"))
   }
 
 }
